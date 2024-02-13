65638bc5a797bedf0f15005a903f5148438fdef0
samiralammumtazalam <samiralammumtazalam@gmail.com>
2024-02-13 16:01:26 +0000

    Updated

--- src/components/ProfileCanvas.jsx
+++ src/components/ProfileCanvas.jsx
@@ -5,7 +5,7 @@ import Navbar from "../components/Navbar";
 const ProfileCanvas = () => {
   return (
     <div>
-      <Navbar />
+      
     </div>
   )
 }
--- src/pages/UserProfile.jsx
+++ src/pages/UserProfile.jsx
@@ -2,9 +2,7 @@ import Navbar from '../components/Navbar'
 
 const UserProfile = () => {
   return (
-    <div>
       <Navbar />
-    </div>
   )
 }
 
