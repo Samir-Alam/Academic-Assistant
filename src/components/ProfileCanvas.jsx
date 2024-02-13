3aaa94ea90df1771f621a71a59f51340db00cbbf
samiralammumtazalam <samiralammumtazalam@gmail.com>
2024-02-13 16:21:05 +0000

    Updated

--- src/components/ProfileCanvas.jsx
+++ src/components/ProfileCanvas.jsx
@@ -1,16 +1,17 @@
 import {motion} from "framer-motion";
-import { textVariant } from "../utils/motion";
+import { fadeIn, textVariant } from "../utils/motion";
 // import Footer from "../components/Footer";
 // import Profile from "../components/Profile";
 
 const ProfileCanvas = () => {
   return (
     <motion.div
-    variants={textVariant()}
+    variants={textVariant(fadeIn("down", "spring", 1, 1))}
     initial="hidden"
     animate="show"
     className="text-center pt-10"
     >
+      My Profile
     </motion.div>
   )
 }
