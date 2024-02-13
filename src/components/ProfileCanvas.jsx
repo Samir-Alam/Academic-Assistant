2fe11a0dde133033bbe3430ed16435e5b784a910
samiralammumtazalam <samiralammumtazalam@gmail.com>
2024-02-13 16:07:39 +0000

    Updated

--- src/components/ProfileCanvas.jsx
+++ src/components/ProfileCanvas.jsx
@@ -1,12 +1,17 @@
 import {motion} from "framer-motion";
+import { textVariant } from "../utils/motion";
 // import Footer from "../components/Footer";
 // import Profile from "../components/Profile";
 
 const ProfileCanvas = () => {
   return (
-    <div>
-      
-    </div>
+    <motion.div
+    variants={textVariant()}
+    initial="hidden"
+    animate="show"
+    className="text-center pt-10"
+    >
+    </motion.div>
   )
 }
 
