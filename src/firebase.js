33f5be50ebbff6cbc21adf1f0d537b40746304fd
samiralammumtazalam <samiralammumtazalam@gmail.com>
2024-02-18 10:30:20 +0000

    Updated

--- src/firebase.js
+++ src/firebase.js
@@ -1,21 +1,36 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-import { getDatabase } from "firebase/database";
+import { getDatabase, ref } from "firebase/database";
+
+// const firebaseConfig = {
+//   apiKey: "AIzaSyCOforGRrhOxYuni04XTwdXfKnoiK6mn5Y",
+//   authDomain: "fir-authentication-9525-1ba12.firebaseapp.com",
+//   projectId: "fir-authentication-9525-1ba12",
+//   storageBucket: "fir-authentication-9525-1ba12.appspot.com",
+//   messagingSenderId: "861350540369",
+//   appId: "1:861350540369:web:6f8011baf566fdcd2fb470",
+//   measurementId: "G-GWTX6Y7YT2",
+// };
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCOforGRrhOxYuni04XTwdXfKnoiK6mn5Y",
-  authDomain: "fir-authentication-9525-1ba12.firebaseapp.com",
-  projectId: "fir-authentication-9525-1ba12",
-  storageBucket: "fir-authentication-9525-1ba12.appspot.com",
-  messagingSenderId: "861350540369",
-  appId: "1:861350540369:web:6f8011baf566fdcd2fb470",
-  measurementId: "G-GWTX6Y7YT2",
+  apiKey: "AIzaSyCrYiq9H7fJEjtWs2b73uZRssbGJM7-d6Y",
+  authDomain: "academic-assistant-0001.firebaseapp.com",
+  databaseURL: "https://academic-assistant-0001-default-rtdb.firebaseio.com",
+  projectId: "academic-assistant-0001",
+  storageBucket: "academic-assistant-0001.appspot.com",
+  messagingSenderId: "625818680665",
+  appId: "1:625818680665:web:5af2ba80c92f611a0be048",
+  measurementId: "G-S5GPNLRJP8"
 };
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
 const auth = getAuth();
+
+const db = getDatabase();
+// const reference = ref(db, "users/" + userId);
 
 export { app, auth };
