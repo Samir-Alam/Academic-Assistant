fee3407a5b292becb66da59bea1a92a0aff5dba1
Samir <samiralammumtazalam@gmail.com>
2024-03-16 13:55:07 +0530

    Updated

---
+++ src/pages/Club_Pages/AddPage.jsx
@@ -0,0 +1,14 @@
+import AddClubCanvas from "../../components/AddClubCanvas"
+import Navbar from "../../components/Navbar"
+
+const AddPage = () => {
+
+  return (
+    <div className="overflow-hidden">
+        <Navbar />
+        <AddClubCanvas />
+    </div>
+  )
+}
+
+export default AddPage
